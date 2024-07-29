f4e879ae0248112aded28bc2cd61e937bdfa168c
stanlapru <stanlap@vk.com>
2024-07-29 19:31:08 +0300

    Add placeholder platformer level - will use this as basis for the real thing

--- resources/tmx/tsx/Overworld.tsx
+++ resources/tmx/tsx/Overworld.tsx
@@ -1,4 +1,88 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
  <image source="Overworld.png" width="640" height="576"/>
+ <tile id="259">
+  <animation>
+   <frame tileid="258" duration="100"/>
+   <frame tileid="259" duration="100"/>
+   <frame tileid="260" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="298">
+  <animation>
+   <frame tileid="298" duration="100"/>
+   <frame tileid="299" duration="100"/>
+   <frame tileid="300" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="338">
+  <animation>
+   <frame tileid="338" duration="100"/>
+   <frame tileid="339" duration="100"/>
+   <frame tileid="340" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="377">
+  <animation>
+   <frame tileid="377" duration="100"/>
+   <frame tileid="417" duration="100"/>
+   <frame tileid="458" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="378">
+  <animation>
+   <frame tileid="378" duration="100"/>
+   <frame tileid="418" duration="100"/>
+   <frame tileid="459" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="379">
+  <animation>
+   <frame tileid="379" duration="100"/>
+   <frame tileid="419" duration="100"/>
+   <frame tileid="460" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="382">
+  <animation>
+   <frame tileid="382" duration="100"/>
+   <frame tileid="385" duration="100"/>
+   <frame tileid="388" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="383">
+  <animation>
+   <frame tileid="383" duration="100"/>
+   <frame tileid="386" duration="100"/>
+   <frame tileid="389" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="384">
+  <animation>
+   <frame tileid="384" duration="100"/>
+   <frame tileid="387" duration="100"/>
+   <frame tileid="390" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="422">
+  <animation>
+   <frame tileid="422" duration="100"/>
+   <frame tileid="425" duration="100"/>
+   <frame tileid="428" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="423">
+  <animation>
+   <frame tileid="423" duration="100"/>
+   <frame tileid="426" duration="100"/>
+   <frame tileid="429" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="424">
+  <animation>
+   <frame tileid="424" duration="100"/>
+   <frame tileid="427" duration="100"/>
+   <frame tileid="430" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
